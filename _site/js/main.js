1862fc83e44dc3842be5906f61f2381346643e78
nihal27 <31623678+nihal27@users.noreply.github.com>
2017-09-20 02:02:09 -0800

    initial commit

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,3 +3,11 @@
 $(".detail-container").on("mouseenter",function (){
    $(this).hide();
 });
+
+// $(".detail-container").on("mouseleave",function (){
+//    $(this).show();
+// });
+
+$(".consultancy-container img").on("mouseenter",function (){
+  $(".consultancy-container .overlay").css("transform","translateY(-450px)");
+});
