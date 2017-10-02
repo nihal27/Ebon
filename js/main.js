c68483b9031659e9989c74db3bbd1bc76d668a17
nihal27 <31623678+nihal27@users.noreply.github.com>
2017-10-03 00:09:37 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+
+$(window).on('load',function(){
+  $('.preloader').fadeOut('slow',function(){
+    $('.overlay').remove();
+  });
+});
+
+
 $('#myTab a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
-})
+});
+
+
+
